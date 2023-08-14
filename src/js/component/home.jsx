fd2121c2647377b5925ed9e576130be72f7e194a
MaxGoxt <135187058+MaxGoxt@users.noreply.github.com>
2023-08-14 18:35:18 +0000

    tarea

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const [toDo, setToDo] = useState([])
+  const [tarea, setTarea] = useState('')
+
+  function add(e) {
+    if (e.key === "Enter" && tarea !== "") {
+      setToDo(toDo.concat(tarea))
+      setTarea("")
+    }}
+
+  function del(item) {
+    setToDo(toDo.filter((x, newIndex) =>
+      item != newIndex
+    ))}
+
+  return (
+    <div className="w-50 mx-auto">
+      <h1 className="contatiner">To Do List</h1>
+      <input className="form-control" type="text" placeholder='¿Que tarea pendiente tenés?'
+        value={tarea}
+        onChange={(e) => { setTarea(e.target.value) }}
+        onKeyDown={add}/>
+      <ul className="list-group">
+        {
+          toDo.map((item, index) => {
+            return (
+              <li className="list-group-item d-flex justify-content-between" key={index}>{item}
+                <i className="fa fa-trash" style={{color: '#c70000'}} onClick={() => del(index)}></i>
+              </li>)
+          })}
+      </ul>
+      <small>{toDo.length} tareas pendientes</small>
+    </div>
+  );
 };
 
 export default Home;
